9f84a0751abf66abf5209876bf6d2b10c0aa9333
CodeforTech2 <stoineaalin2501@gmail.com>
2022-10-05 20:09:08 +0300

    Adding resetAll and restEntry function

--- calculator.js
+++ calculator.js
@@ -1,8 +1,26 @@
 const btns = document.querySelectorAll('.btn');
 const currentResult = document.querySelector('.current-result');
-
+const previousResult = document.querySelector('.previous-result');
+const clearAll = document.querySelector('#clear-all');
+const clearEntry = document.querySelector('#clear-entry');
 
 btns.forEach(btn => btn.addEventListener('click', (e) => {
     console.log(e.target.id);
-    currentResult.innerHTML = e.target.id;
-}))
+    const firstNumber = e.target.innerHTML; 
+    currentResult.innerHTML += firstNumber;
+}));
+
+function resetAll() {
+    clearAll.addEventListener('click', () => {
+        currentResult.innerHTML = '';
+        previousResult.innerHTML = '';
+    });
+};
+resetAll();
+
+function resetEntry() {
+    clearEntry.addEventListener('click', () => {
+        currentResult.innerHTML = '';
+    })
+}
+resetEntry();
