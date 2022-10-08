4f2f2110931a3d0b0d43d8f3d3ef05a6e0aed31b
CodeforTech2 <stoineaalin2501@gmail.com>
2022-10-08 09:40:43 +0300

    Try to make calculate function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,6 @@
+let firstOperand = '';
+let secondOperand = '';
+
 const btns = document.querySelectorAll('.btn');
 const currentResult = document.querySelector('.current-result');
 const previousResult = document.querySelector('.previous-result');
@@ -8,15 +11,30 @@ btns.forEach(btn => btn.addEventListener('click', (e) => {
     console.log(e.target);
     const firstNumber = e.target.innerHTML; 
     currentResult.innerHTML += firstNumber;
-   
+    
     
 }));
 
 function matchKey(e) {
     const number = document.querySelector(`button[key = "${e.key}"]`);
     if (!number) return;
-    console.log(number);
-    currentResult.innerHTML = number.textContent;
+    // console.log(number);
+    // currentResult.innerHTML += number.textContent;
+    // console.log(currentResult.innerHTML);
+    // console.log(number.classList.contains('number'));
+    if (number.classList.contains('number')) {
+        currentResult.innerHTML += number.textContent;
+        // console.log(currentResult.innerHTML);
+    } else if (number.classList.contains('operator')) {
+        const firstValue = currentResult.innerHTML;
+        const operator = number.innerHTML;
+        currentResult.innerHTML += number.textContent;
+        // console.log(firstValue);
+        // console.log(operator);
+    } else if (number.classList.contains('number')) {
+        const secondValue = currentResult.innerHTML;
+        console.log(secondValue);
+    }
 }
 
 
@@ -25,7 +43,7 @@ window.addEventListener('keydown', matchKey);
 // Function to reset all calculation made before
 function resetAll() {
     clearAll.addEventListener('click', () => {
-        currentResult.innerHTML = '';
+        currentResult.innerHTML = 0;
         previousResult.innerHTML = '';
     });
 };
