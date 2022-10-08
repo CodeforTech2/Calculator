0b373ccd6c4acb2a36892d12584b63662d47470e
CodeforTech2 <stoineaalin2501@gmail.com>
2022-10-08 17:09:06 +0300

    Resolve appendNumber function

--- calculator.js
+++ calculator.js
@@ -10,33 +10,35 @@ const clearEntry = document.querySelector('#clear-entry');
 btns.forEach(btn => btn.addEventListener('click', (e) => {
     console.log(e.target);
     const firstNumber = e.target.innerHTML; 
-    currentResult.innerHTML += firstNumber;
-    
-    
+    // currentResult.innerHTML += firstNumber;
 }));
 
 function matchKey(e) {
     const number = document.querySelector(`button[key = "${e.key}"]`);
     if (!number) return;
-    // currentResult.innerHTML += number.textContent;
-    // console.log(currentResult.innerHTML);
-    console.log(number.classList.contains('number'));
-    if (number.classList.contains('number')) {
-        currentResult.innerHTML += number.textContent;
-        // console.log(currentResult.innerHTML);
-    } else if (number.classList.contains('operator')) {
-        const firstValue = currentResult.innerHTML;
-        const operator = number.innerHTML;
-        currentResult.innerHTML += number.textContent;
-        // console.log(firstValue);
-        // console.log(operator);
-    } else if (number.classList.contains('number')) {
-        const secondValue = currentResult.innerHTML;
-        console.log(secondValue);
-    }
+    // console.log(number.textContent);
+    appendNumber(number.textContent);
+    
+    // if (number.classList.contains('number')) {
+    //     // console.log(currentResult.innerHTML);
+    //     appendNumber(number);
+    // } 
 }
 
 window.addEventListener('keydown', matchKey);
+
+//Append number to currentResults
+function appendNumber(nr) {
+    if (currentResult.textContent === '0') {
+        currentResult.textContent = '';
+    }
+    currentResult.textContent += nr;
+}
+
+//Reset screen/eliminate 0 to start adding numbers
+function resetScreen() {
+    currentResult.textContent = '';
+}
 
 // Function to reset all calculation made before
 function resetAll() {
